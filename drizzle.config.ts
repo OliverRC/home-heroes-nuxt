a373d3e6f6aea19960e7c1f6325dd89e4c531e78
Oliver Rivett-Carnac <oliverrc@gmail.com>
2024-04-22 12:15:35 +0200

    Trying to get vercel to do the migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'drizzle-kit'
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export default defineConfig({
   schema: './server/database/schema.ts',
   out: './server/database/migrations',
   driver: 'pg',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!
+    connectionString: process.env.DATABASE_URL! + '?sslmode=require'
   },
 })
